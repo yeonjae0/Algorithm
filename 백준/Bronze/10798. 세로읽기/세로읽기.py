79f902889cd484ff23e4ceaaf594fed02db36ac9
yeonjae0 <122508627+yeonjae0@users.noreply.github.com>
2023-03-02 00:03:54 +0900

    [Bronze I] Title: 세로읽기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -0,0 +1,20 @@
+arr = [input() for _ in range(5)]
+
+cnt = 0
+for i in arr:
+    if cnt < len(i):
+        cnt = len(i)
+arr2 = []
+for st in arr:
+    while len(st) != cnt:
+        st += '*'
+    arr2.append(st)
+
+arr_ = list(map(list, zip(*arr2)))
+
+ans = ''
+for st1 in arr_:
+    for st2 in st1:
+        if st2 != '*':
+            ans += st2
+print(ans)
