6572dc1d577262d273bd707c7ff5c9dbf8d291a1
yeonjae0 <122508627+yeonjae0@users.noreply.github.com>
2023-03-02 11:15:32 +0900

    [Silver II] Title: 창고 다각형, Time: 80 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.py
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.py
@@ -1,39 +1,59 @@
+# 기둥의 개수 N (1<=N<=1000)
 N = int(input())
-
-i_lst = []  # l값 넣어줌
-# h_lst = []  # h값 넣어줌
-lst = []
+lst = [0] * 1001
+mx_height = mx_idx = 0 # 가장 높은 기둥이 나오는 인덱스
 for _ in range(N):
-    l, h = map(int, input().split())
-    lst.append([l, h])
-    i_lst.append(l)
-    # h_lst.append(h)
-height = [0] * (max(i_lst) + 1)
+    # 왼쪽 면의 위치 L, 높이 H (1<= _ <=1000)
+    L, H = map(int, input().split())
+    lst[L] = H
+    mx_height = max(mx_height, H)
+mx_idx = lst.index(mx_height)
 
-# height 해당하는 위치에 먼저 높이들 넣어 주기
-for i in lst:
-    height[i[0]] += i[1]
-# 가장 높은 곳의 위치값 (인덱스값)
-max_idx = height.index(max(height))
+# 첫번째 나오는 가장 큰 기둥을 기준으로 두 구간으로 나누기
+lst_a = lst[:mx_idx + 1]
+lst_b = lst[mx_idx:]
 
-left = 0  # 왼쪽 포인터
-right = len(height) - 1  # 오른쪽 포인터
-left_num = 0  # 최신화 해줄 숫자
-
-while left <= max_idx:
-    if height[left] > left_num:
-        left_num = height[left]
+# 왼쪽에서 빛 쐈을 때
+current_height = lst_a[0]
+area = 0
+for i in range(1, len(lst_a)):
+   # 앞은 0이었고 더 높은 기둥을 만났을 경우 (처음 시작할 때)
+    if current_height == 0 and lst_a[i] >= current_height:
+        # 높이 갱신
+        current_height = lst_a[i]
+        # 넓이에 더하기
+        area += current_height
+    # 0을 지나서 기둥 갱신되었는데 또 더 높은 기둥 만난 경우
+    elif current_height != 0 and lst_a[i] >= current_height:
+        # 더 높은 기둥으로 높이 갱신
+        current_height = lst_a[i]
+        # 넓이에 더하기
+        area += current_height
+    # 이전 기둥이 0이 아니었는데 중간에 0을 만난 경우 (오목하게 들어간 경우)
+    elif current_height != 0 and lst_a[i] == 0:
+        # 오목한 부분 상관없이 지금 가지고 있는 높이 더하기
+        area += current_height
+    # 이전 기둥 0이 아니었고 더 작은 기둥 만난 경우
+    elif current_height != 0 and lst_a[i] < current_height:
+        # 갱신 안하고 넓이에 더하기
+        area += current_height
     else:
-        height[left] = left_num
-    left += 1
+        pass
 
-right_num = 0
-while right > max_idx:
-    if height[right] > right_num:
-        right_num = height[right]
+current_height = lst_b[-1]
+for i in range(len(lst_b) - 1, -1, -1):
+    # 저장된 높이보다 높으면
+    if current_height == 0 and lst_b[i] >= current_height:
+        current_height = lst_b[i]
+        area += current_height
+    elif current_height != 0 and lst_b[i] >= current_height:
+        current_height = lst_b[i]
+        area += current_height
+    elif current_height != 0 and lst_b[i] == 0:
+        area += current_height
+    elif current_height != 0 and lst_b[i] < current_height:
+        area += current_height
     else:
-        height[right] = right_num
-    right -= 1
+        pass
 
-print(sum(height))
-
+print(area - mx_height)
