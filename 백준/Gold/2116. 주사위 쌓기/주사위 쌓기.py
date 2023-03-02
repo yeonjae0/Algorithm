3cc4d01a18129bad15799a1a7073ee158e823f3e
yeonjae0 <122508627+yeonjae0@users.noreply.github.com>
2023-03-02 12:15:35 +0900

    [Gold V] Title: 주사위 쌓기, Time: 512 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Gold/2116. 주사위 쌓기/주사위 쌓기.py
+++ 백준/Gold/2116. 주사위 쌓기/주사위 쌓기.py
@@ -1,51 +1,44 @@
-# 2116 백준 주사위 쌓기
+def side(index):
+    if index == (0, 5) or index == (5, 0):
+        return [1, 2, 3, 4]
+    elif index == (1, 4) or index == (4, 1):
+        return [0, 2, 3, 5]
+    else:
+        return [0, 1, 4, 5]
 
-dic = {0: 5, 1: 3, 2: 4, 3: 1, 4: 2, 5: 0}
-
-
-def bot_to_top(llst, bot_num):
-    # 리스트 내에서 아래 숫자를 찾고 해당하는 위치 파악 후
-    # 다음 탑 숫자를 반환
-    global next_top
-    for i in range(len(llst)):
-        if llst[i] == bot_num:
-            next_top = llst[dic[i]]
-
-            
-def dice_idx_max(lst, num):
-    # top 숫자를 1차원 리스트 lst에서 찾아서 해당 리스트 내의 인덱스를 찾고
-    # (0, 5), (1, 3), (2,4) 묶어서 제외한 후 나머지 중에 최대를 반환
-    for k in lst:
-        if k == num:
-            if lst.index(k) in (0, 5):
-                tmp_lst = [lst[1], lst[2], lst[3], lst[4]]
-
-            elif lst.index(k) in (1, 3):
-                tmp_lst = [lst[2], lst[0], lst[4], lst[5]]
-            else:
-                tmp_lst = [lst[0], lst[1], lst[3], lst[5]]
-    #print(tmp_lst)
-    m_num = max(tmp_lst)
-    return m_num
-
-
-
+# 주사위 개수 N
 N = int(input())
-dice = [list(map(int, input().split())) for _ in range(N)]
-#print(dice)
-# tmp_ans = 0 #최대를 계속 더해줌
-ans = []  # 최댓값의 합들을 보관
-next_top = 0  # 윗면에 올 숫자
+dices = []
+dices_extra = []
+for _ in range(N):
+    lst = list(map(int, input().split()))
+    # dices.append(lst)
+    lst[3], lst[4] = lst[4], lst[3]
+    dices.append(lst)
+    # dices_extra.append(lst)
 
-for i in dice[0]:  # 첫 주사위 돌 때 위에 오는 수
-    next_top = i
-    tmp_ans = 0
-    # i는 다음 주사위 아래 숫자와 같고 next_top은 다음 주사위 위에 있을 숫자
-    for j in range(0, N):  # N개의 주사위를 돈다
-        #print(dice_idx_max(dice[j], next_top))
-        tmp_ans += dice_idx_max(dice[j], next_top)
-        bot_to_top(dice[j], next_top)
+max_candidates = []
+sum_max = 0
+for i in range(6):
+    sum_max = 0
+    # 첫 주사위 위 아래를 임의로 정해준다
+    bottom = dices[0][i]
+    top = dices[0][5-i]
+    # 위 아래를 제외하고 될 수 있는 인덱스
+    side_idx = side((i, 5-i))
+    # 옆에서 올 수있는 가장 큰 수
+    side_mx = max([dices[0][i] for i in side_idx])
+    sum_max += side_mx
+    next_bottom = top
+    for j in range(1, N):
+        # 다음 숫자의 바텀 인덱스
+        bottom_idx = dices[j].index(next_bottom)
+        # 다음 숫자의 바텀이 특정 숫자일 때 올 수 있는 옆면의 인덱스들
+        side_idx = side((bottom_idx, 5 - bottom_idx))
+        # 옆면 인덱스 중에 가장 큰 수
+        side_mx = max([dices[j][i] for i in side_idx])
+        sum_max += side_mx
+        next_bottom = dices[j][5 - bottom_idx]
+    max_candidates.append(sum_max)
 
-    ans.append(tmp_ans)
-
-print(max(ans))
+print(max(max_candidates))
