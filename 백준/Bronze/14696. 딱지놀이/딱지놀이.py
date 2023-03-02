31b6cba3823210a1bdfb14a49a47a1a316de5642
yeonjae0 <122508627+yeonjae0@users.noreply.github.com>
2023-03-02 10:10:41 +0900

    [Bronze I] Title: 딱지놀이, Time: 192 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/14696. 딱지놀이/딱지놀이.py
@@ -0,0 +1,40 @@
+# 14696 백준 딱지놀이
+
+def win(alst, blst):
+    if alst.count(4) > blst.count(4):
+        return print('A')
+    elif alst.count(4) < blst.count(4):
+        return print('B')
+
+    elif (alst.count(4) == blst.count(4)) and alst.count(3) > blst.count(3):
+        return print('A')
+    elif (alst.count(4) == blst.count(4)) and alst.count(3) < blst.count(3):
+        return print('B')
+
+    elif (alst.count(4) == blst.count(4)) and (alst.count(3) == blst.count(3)) \
+            and (alst.count(2) > blst.count(2)):
+        return print('A')
+    elif (alst.count(4) == blst.count(4)) and (alst.count(3) == blst.count(3)) \
+         and (alst.count(2) < blst.count(2)):
+        return print('B')
+
+    elif (alst.count(4) == blst.count(4)) and (alst.count(3) == blst.count(3)) \
+        and (alst.count(2) == blst.count(2)) and (alst.count(1) > blst.count(1)):
+        return print('A')
+    elif (alst.count(4) == blst.count(4)) and (alst.count(3) == blst.count(3)) \
+         and (alst.count(2) == blst.count(2)) and (alst.count(1) < blst.count(1)):
+        return print('A')
+
+    else:
+        return print('D')
+
+
+
+T = int(input())
+for _ in range(T):
+    A = list(map(int, input().split()))
+    del A[0]
+    B = list(map(int, input().split()))
+    del B[0]
+
+    win(A, B)
