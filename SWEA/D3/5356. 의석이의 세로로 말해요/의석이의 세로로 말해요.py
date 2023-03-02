7f63ceffbbb61d58129d2917e1c17a1c75fb63e8
yeonjae0 <122508627+yeonjae0@users.noreply.github.com>
2023-03-02 09:20:38 +0900

    [D3] Title: 의석이의 세로로 말해요, Time: 203 ms, Memory: 61,716 KB -BaekjoonHub

---
+++ SWEA/D3/5356. 의석이의 세로로 말해요/의석이의 세로로 말해요.py
@@ -0,0 +1,22 @@
+T = int(input())
+for tc in range(1, T+1):
+    arr = [input() for _ in range(5)]
+
+    cnt = 0
+    for i in arr:
+        if cnt < len(i):
+            cnt = len(i)
+    arr2 = []
+    for st in arr:
+        while len(st) != cnt:
+            st += '*'
+        arr2.append(st)
+
+    arr_ = list(map(list, zip(*arr2)))
+    ans = ''
+    for st1 in arr_:
+        for st2 in st1:
+            if st2 != '*':
+                ans += st2
+                
+    print(f'#{tc} {ans}')
